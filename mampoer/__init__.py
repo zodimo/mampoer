ef84c032d57aaf7fe6f28549ebede3f4333d0f0a
Jaco Labuschagne <zodimo@gmail.com>
2020-05-26 22:40:50 +0200

    Bump version: 0.1.2 → 0.1.3

--- mampoer/__init__.py
+++ mampoer/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Jaco Labuschagne"""
 __email__ = "zodimo@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .mampoer import Mampoer
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/zodimo/mampoer",
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
